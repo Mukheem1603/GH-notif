1617752debbf8a2cdd88c52714baf3585d14c5c2
Mukheem <mukheem1603@gmail.com>
2020-07-26 12:26:39 +0530

    added files.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import requests
 from discord.ext import commands
 
 
@@ -9,7 +10,12 @@ bot = commands.Bot(command_prefix='$')
 async def test(ctx, arg):
     await ctx.send(arg)
 
-    
+@bot.command()
+async def info(ctx, arg):
+    r = requests.get(f"https://api.github.com/users/{arg}")
+    resp = r.json()
+    await ctx.send(resp['name'])
+
 bot.run(os.environ['TOKEN'])
 
 
