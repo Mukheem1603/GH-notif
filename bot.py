c7b8891f37045dd5865b16428b2b92aa68275eea
Mukheem <mukheem1603@gmail.com>
2020-07-26 11:45:46 +0530

    added files.

--- bot.py
+++ bot.py
@@ -9,5 +9,27 @@ bot = commands.Bot(command_prefix='$')
 async def test(ctx, arg):
     await ctx.send(arg)
 
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Heyaa {member.name}, welcome!!!'
+    )
+
+
+client.run(os.environ['TOKEN'])
 
 bot.run(os.environ['TOKEN'])
