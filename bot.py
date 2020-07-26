9edc85b164aa0a77655fe32dc8ba0a93c04ff9ee
Mukheem <mukheem1603@gmail.com>
2020-07-26 12:21:34 +0530

    added files.

--- bot.py
+++ bot.py
@@ -2,32 +2,14 @@ import discord
 import os
 from discord.ext import commands
 
-client = discord.Client()
+
 bot = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
-
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Heyaa {member.name}, welcome!!!'
-    )
-
-@commands.command()
+@bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
 
-bot.add_command(test)
+    
+bot.run(os.environ['TOKEN'])
 
-client.run(os.environ['TOKEN'])
+
